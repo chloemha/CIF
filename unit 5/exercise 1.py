8be422327c4f6c3492ce195aa41feaf125986662
chloemha <chloe.m.ha@gmail.com>
2025-11-09 17:35:55 -0500

    Add files via upload

---
+++ unit 5/exercise 1.py
@@ -0,0 +1,10 @@
+listt=[
+    ["True/False: Europe has no deserts", ["true", "True"]]
+    ["How many seasons does Friends have?", ["10", "ten", "Ten"]]
+    ["What is the largest organ?", ["skin", "Skin"]]
+    ["What is the most copied link on the internet? (just one word)", ["rickroll", "Rickroll", "RickRoll"]]
+    ["True/False: Albert Einstein had trouble with math in school", ["false", "False"]]
+    ["How many hearts does an octopus have?", ["3", "three", "Three"]]
+]
+
+print(listt[0][2])
