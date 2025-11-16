29097c52a021fba98324bc9c192672bea40b75e9
chloemha <chloe.m.ha@gmail.com>
2025-11-15 19:19:05 -0500

    Update unit 4 homework part 1.py

--- unit 4/unit 4 homework part 1.py
+++ unit 4/unit 4 homework part 1.py
@@ -1,2 +1,4 @@
-def game():
-    print("Welcome to the Harry Potter quiz! \n Answer each question correctly to gain points!")
+def beginning():
+    instructions= ["\nWelcome to the Harry Potter game!", "Instructions:", "1. You have recieved your Hogwarts letter, and you have taken the train to Hogwarts", "2. Go to the sorting ceremony to get sorted into your house", "3. Take the following class:", "- charms/defense against the dark arts", "4. Answer questions corectly to earn points!"]
+    for i in instructions:
+        print(i)
